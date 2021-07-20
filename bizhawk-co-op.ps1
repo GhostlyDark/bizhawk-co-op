bc643ad1af04ad4f5bbb99d4ca96f6b0855f7349
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2021-07-20 14:19:37 +0200

    Use custom rumble-HTS build

--- bizhawk-co-op.ps1
+++ bizhawk-co-op.ps1
@@ -2,17 +2,17 @@
 
 $shell_app=new-object -com shell.application
 
-mkdir BizHawk-2.6.1
+mkdir BizHawk
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 #Download Bizhawk
-$url = "https://github.com/TASVideos/BizHawk/releases/download/2.6.1/BizHawk-2.6.1-win-x64.zip"
-$filename = "bizHawk-2.6.1.zip"
+$url = "https://github.com/GhostlyDark/BizHawk/releases/download/2021-03/BizHawk-rumble-HTS.zip"
+$filename = "bizhawk.zip"
 Invoke-WebRequest -Uri $url -OutFile $filename
 #Unzip
 $zip_file = $shell_app.namespace((Get-Location).Path + "\$filename")
-$destination = $shell_app.namespace((Get-Location).Path + "\BizHawk-2.6.1")
+$destination = $shell_app.namespace((Get-Location).Path + "\BizHawk")
 $destination.Copyhere($zip_file.items())
 Remove-Item $filename
 
@@ -48,13 +48,13 @@ $destination.Copyhere($zip_file.items())
 Remove-Item $filename
 
 #Copy files into Bizhawk
-Move-Item -Path .\bizhawk-co-op-master\* -Destination .\BizHawk-2.6.1\
+Move-Item -Path .\bizhawk-co-op-master\* -Destination .\BizHawk\
 Remove-Item .\bizhawk-co-op-master -Recurse
 
-Move-Item -Path .\luasocket\mime -Destination .\BizHawk-2.6.1\
-Move-Item -Path .\luasocket\socket -Destination .\BizHawk-2.6.1\
-Move-Item -Path .\luasocket\lua\* -Destination .\BizHawk-2.6.1\Lua\
-Move-Item -Path .\luasocket\lua5.1.dll -Destination .\BizHawk-2.6.1\dll\
+Move-Item -Path .\luasocket\mime -Destination .\BizHawk\
+Move-Item -Path .\luasocket\socket -Destination .\BizHawk\
+Move-Item -Path .\luasocket\lua\* -Destination .\BizHawk\Lua\
+Move-Item -Path .\luasocket\lua5.1.dll -Destination .\BizHawk\dll\
 Remove-Item .\luasocket -Recurse
 
 Start-Process .\bizhawk_prereqs.exe -Wait
