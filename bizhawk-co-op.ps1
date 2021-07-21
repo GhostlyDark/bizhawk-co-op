98e3367dee1f1e39027498bd6c6cd922776d3183
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2021-07-21 16:04:15 +0200

    Update BizHawk

--- bizhawk-co-op.ps1
+++ bizhawk-co-op.ps1
@@ -7,7 +7,7 @@ mkdir BizHawk
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 #Download Bizhawk
-$url = "https://github.com/GhostlyDark/BizHawk/releases/download/2021-03/BizHawk-rumble-HTS.zip"
+$url = "https://github.com/GhostlyDark/BizHawk/releases/download/2021-07-21/BizHawk_Developer-2021-07-21.zip"
 $filename = "bizhawk.zip"
 Invoke-WebRequest -Uri $url -OutFile $filename
 #Unzip
